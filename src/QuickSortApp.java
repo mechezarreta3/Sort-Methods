d9c4ed2c7a496d9c486c2b6d998e5af8adf64d8f
Modesto Echezarreta <echezarreta.m@gmail.com>
2020-06-14 17:16:08 -0500

    Fixed compilation error

--- src/QuickSortApp.java
+++ src/QuickSortApp.java
@@ -1,26 +1,21 @@
 public class QuickSortApp {
-    public static void quickSortArray(int[] array, int low, int high)
-    {
-        if (array.length != 1)
-        {
+    public static void quickSortArray(int[] array, int low, int high) {
+        if (array.length != 1) {
             int p = partition(array, low, high);
 
             // Recursively sort elements before pivot point
             // and after pivot point
             quickSortArray(array, low, p);
-            quickSortArray(array, p + 1, high;
+            quickSortArray(array, p + 1, high);
         }
     }
 
-    public static int partition(int[] array, int low, int high)
-    {
+    public static int partition(int[] array, int low, int high) {
         int p = low;
         int temp;
 
-        for (int i = low + 1; i < high; i++)
-        {
-            if(array[i] < array[low])
-            {
+        for (int i = low + 1; i < high; i++) {
+            if(array[i] < array[low]) {
                 p++;
                 temp = array[i];
                 array[i] = array[low];
