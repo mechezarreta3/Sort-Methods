7ce164ba23a4374b221c4d1befbc8d243cca65b5
Modesto Echezarreta <echezarreta.m@gmail.com>
2020-06-14 16:03:02 -0500

    Added documentation to BubbleSortApp.

--- src/BubbleSortApp.java
+++ src/BubbleSortApp.java
@@ -4,6 +4,10 @@ public class BubbleSortApp {
     public static void bubbleSortArray(int[] integerArray) {
         int temp;
 
+        /**
+         * Purpose: Traverse the array using two 'for' loops. Each pass of the nested for loop will result in pushing
+         * the largest value found to the last index that has not been updated/left alone.
+         */
         for(int i = 0; i < integerArray.length; i++) {
             for(int j = 1; j < integerArray.length - i; j++) {
                 if (integerArray[j] < integerArray[j - 1]) {
@@ -19,13 +23,13 @@ public class BubbleSortApp {
         //Array used for testing that sort method worked successfully
         int[] testArray = { 1, 2, 3, 3, 2, 1 };
 
-        //Purpose: Display initial testArray to the screen, using System.out.println and Arrays.toString method
+        //Display initial testArray to the screen, using System.out.println and Arrays.toString method
         System.out.println("Initial Array:\n" + Arrays.toString(testArray));
 
         //Sort testArray using bubble sort method
         bubbleSortArray(testArray);
 
-        ////Purpose: Display initial testArray to the screen, using System.out.println and Arrays.toString method
+        //Display initial testArray to the screen, using System.out.println and Arrays.toString method
         System.out.println("\nSorted Array:\n" + Arrays.toString(testArray));
     }
 }
