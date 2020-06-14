959f2af60082b0f91c43a1a989ba84e459b7c682
Modesto Echezarreta <echezarreta.m@gmail.com>
2020-06-14 17:14:23 -0500

    Implement partition method

--- src/QuickSortApp.java
+++ src/QuickSortApp.java
@@ -1,14 +1,36 @@
 public class QuickSortApp {
-    public static void quickSort(int[] array, int low, int high)
+    public static void quickSortArray(int[] array, int low, int high)
     {
         if (array.length != 1)
         {
             int p = partition(array, low, high);
 
-            // Recursively sort elements before
-            // partition and after partition
-            quickSort(array, low, p);
-            quickSort(array, p + 1, high;
+            // Recursively sort elements before pivot point
+            // and after pivot point
+            quickSortArray(array, low, p);
+            quickSortArray(array, p + 1, high;
         }
+    }
+
+    public static int partition(int[] array, int low, int high)
+    {
+        int p = low;
+        int temp;
+
+        for (int i = low + 1; i < high; i++)
+        {
+            if(array[i] < array[low])
+            {
+                p++;
+                temp = array[i];
+                array[i] = array[low];
+                array[low] = temp;
+            }
+        }
+        temp = array[low];
+        array[low] = array[p];
+        array[p] = temp;
+
+        return p;
     }
 }
